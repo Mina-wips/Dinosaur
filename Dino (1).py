1815bc0163273c5b3a54d305f1317d50f1e57c86
Mina-wips <143671063+Mina-wips@users.noreply.github.com>
2023-11-11 17:22:53 +0000

    First Commit

---
+++ Dino (1).py
@@ -0,0 +1,19 @@
+print("roarrrrr")
+print("does the dinosaur eat plant? ")
+if input() == "no":
+    print("does the dinosaur have tiny arms")
+if input() =="yes":
+  print("tyranusorous")
+else:
+  print("does the dinosaur chew")
+if input() =="no":
+  print("does the dinosaur have long leg and hand?")
+if input() == "yes":
+  print("Saurischians")
+  print("runnnnnn")
+  
+else:
+  print("does the dinosaurr have a long neck:")
+if input() =="yes":
+print("the dinosaur is velociprator")
+print("you are safe")
